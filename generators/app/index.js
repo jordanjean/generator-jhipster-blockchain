0e6fa22998f35a2f0ae35f3204fff600802d1083
Jordan JEAN <jordan.jean73540@gmail.com>
2019-03-13 16:23:46 +0100

    introduce blockchain operations in request creation

--- generators/app/index.js
+++ generators/app/index.js
@@ -89,6 +89,9 @@ module.exports = class extends BaseGenerator {
 
         // Write JDL file
         this.template('blockchain-jdl.jh', 'blockchain-jdl.jh');
+
+        // Write RequestRessource.java file
+        this.template('RequestResource.java', `${javaDir}/web/rest/RequestResource.java`);
     }
 
     install() {
@@ -236,6 +239,13 @@ module.exports = class extends BaseGenerator {
             splicable: [`package ${packageName}.network;\n`]
         }, this);
 
+        // Write package statement
+        jhipsterUtils.rewriteFile({
+            file: `${javaDir}/web/rest/RequestResource.java`,
+            needle: 'import java.net.URI;',
+            splicable: [`package ${packageName}.web.rest;\n`]
+        }, this);
+
         // Write imports
         jhipsterUtils.rewriteFile({
             file: `${javaDir}network/request/A_BlockchainRequest.java`,
@@ -299,242 +309,22 @@ import ${packageName}.network.networkException.StateAlreadySet;\n`]
             splicable: [`<logger name="io" level="WARN"/>
 <logger name="i.n.h.c.http2.Http2ConnectionHandler" level="WARN"/>\n`]}, this);
 
-        // Write REST controller imports
+        // Write imports
         jhipsterUtils.rewriteFile({
             file: `${javaDir}web/rest/RequestResource.java`,
-            needle: `import io.github.jhipster.web.util.ResponseUtil;`,
-            splicable: [`import java.time.Instant;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import ${packageName}.domain.enumeration.RequestType;
+            needle: 'import io.github.jhipster.web.util.ResponseUtil;',
+            splicable: [`import ${packageName}.domain.Request;
 import ${packageName}.network.networkException.A_BlockchainException;
 import ${packageName}.network.networkException.EntityNotFound;
 import ${packageName}.network.request.Add;
 import ${packageName}.network.request.Delete;
 import ${packageName}.network.request.Get;
 import ${packageName}.network.request.Set;
+import ${packageName}.repository.RequestRepository;
 import ${packageName}.web.rest.errors.BadRequestAlertException;
-import ${packageName}.web.rest.util.HeaderUtil;
-
-import io.github.jhipster.web.util.ResponseUtil;\n`]}, this);
-
-        // Write blockchain requests functions in REST controller
-        jhipsterUtils.rewriteFile({
-            file: `${javaDir}web/rest/RequestResource.java`,
-            needle: '}',
-            splicable: [`	/**
-	 * POST /requests/add : add a new value to the blockchain.
-	 *
-	 * @param value the hash of the diploma we want to add to the BC
-	 * @return the ResponseEntity with status 200 (OK) and the transaction ID, or
-	 *         with status 417 (EXPECTATION_FAILED), or with status 500
-	 *         (INTERNAL_SERVER_ERROR)
-	 */
-	@PostMapping("/requests/add")
-	public ResponseEntity<String> addRequest(@RequestParam String entity, String value) {
-		if (entity.isEmpty()) {
-			log.debug("Empty entity name");
-			return new ResponseEntity<String>("EMPTY_ENTITY_NAME", HttpStatus.EXPECTATION_FAILED);
-		}
-		if (value.isEmpty()) {
-			log.debug("Empty value");
-			return new ResponseEntity<String>("EMPTY_VALUE", HttpStatus.EXPECTATION_FAILED);
-		}
-
-		Add blockchainRequest;
-		String transactionID;
-		try {
-			blockchainRequest = new Add(entity, value);
-			blockchainRequest.send();
-			transactionID = blockchainRequest.transactionID;
-		} catch (A_BlockchainException e) {
-			String errored = "BLOCKCHAIN ERROR: " + e.toString();
-			log.debug(errored);
-			return new ResponseEntity<String>(errored, HttpStatus.NOT_ACCEPTABLE);
-		} catch (Exception e) {
-			String errored = "BLOCKCHAIN ERROR: " + e.toString();
-			log.debug(errored);
-			return new ResponseEntity<String>(errored, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-
-		// Log the insert operation (whether fail or not)
-		Instant timestamp = Instant.now();
-		Request request = new Request();
-		request.setValue(value);
-		request.setType(RequestType.ADDITION);
-		request.setDate(timestamp);
-		requestRepository.save(request);
-
-		// Create JSON string
-		String returned = "{" + '"' + "transactionID" + '"' + ":" + '"' + transactionID + '"' + "}";
-		return new ResponseEntity<String>(returned, HttpStatus.OK);
-	}
-
-	/**
-	 * GET /requests/get : Get an entity value from the blockchain
-	 *
-	 * @param entity the entity to query
-	 * @return the ResponseEntity with status 200 (OK) and the value of the entity,
-	 *         or with status 417 (EXPECTATION_FAILED), or with status 500
-	 *         (INTERNAL_SERVER_ERROR)
-	 */
-	@GetMapping("/requests/get")
-	public ResponseEntity<String> getRequest(@RequestParam String entity) {
-		if (entity.isEmpty()) {
-			log.debug("Empty entity name");
-			return new ResponseEntity<String>("EMPTY_ENTITY_NAME", HttpStatus.EXPECTATION_FAILED);
-		}
-
-		String value = null;
-		Get get;
-
-		// log the consultation operation
-		Instant timestamp = Instant.now();
-		Request request = new Request();
-		request.setValue(entity);
-		request.setType(RequestType.GET);
-		request.setDate(timestamp);
-		requestRepository.save(request);
-
-		try {
-			get = new Get(entity);
-			get.send();
-			value = get.state;
-		} catch (EntityNotFound e) {
-			String errored = "BLOCKCHAIN ERROR: " + e.toString();
-			log.debug(errored);
-
-			// Create JSON string
-			String returned = "{" + '"' + "entityState" + '"' + ":" + '"' + "NOT_FOUND" + '"' + "}";
-			return new ResponseEntity<String>(returned, HttpStatus.OK);
-		} catch (Exception e) {
-			String errored = "BLOCKCHAIN ERROR: " + e.toString();
-			log.debug(errored);
-			return new ResponseEntity<String>(errored, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-
-		if (value == null) {
-			log.debug("The query has failed");
-			return new ResponseEntity<String>("QUERY FAILED", HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-		switch (value) {
-		case "NOT_FOUND":
-			// Create JSON string
-			String returned = "{" + '"' + "entityState" + '"' + ":" + '"' + "NOT_FOUND" + '"' + "}";
-			return new ResponseEntity<String>(returned, HttpStatus.OK);
-		}
-
-		// Create JSON string
-		String returned = "{" + '"' + "entityState" + '"' + ":" + '"' + value + '"' + "}";
-		return new ResponseEntity<String>(returned, HttpStatus.OK);
-	}
-
-	/**
-	 * DELETE /requests/delete : delete an entity from the blockchain.
-	 *
-	 * @param entity to delete from the blockchain
-	 * @return the ResponseEntity with status 200 (OK) and the transaction ID, or
-	 *         with status 417 (EXPECTATION_FAILED), or with status 500
-	 *         (INTERNAL_SERVER_ERROR)
-	 */
-	@DeleteMapping("/requests/delete")
-	public ResponseEntity<String> deleteRequest(@RequestParam String entity) {
-		if (entity.isEmpty()) {
-			log.debug("Empty entity name");
-			return new ResponseEntity<String>("EMPTY_ENTITY_NAME", HttpStatus.EXPECTATION_FAILED);
-		}
-
-		Delete blockchainRequest;
-		String transactionID;
-		try {
-			blockchainRequest = new Delete(entity);
-			blockchainRequest.send();
-			transactionID = blockchainRequest.transactionID;
-		} catch (A_BlockchainException e) {
-			String errored = "BLOCKCHAIN ERROR: " + e.toString();
-			log.debug(errored);
-			return new ResponseEntity<String>(errored, HttpStatus.NOT_ACCEPTABLE);
-		} catch (Exception e) {
-			String errored = "BLOCKCHAIN ERROR: " + e.toString();
-			log.debug(errored);
-			return new ResponseEntity<String>(errored, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-
-		// Log the insert operation (whether fail or not)
-		Instant timestamp = Instant.now();
-		Request request = new Request();
-		request.setValue(entity);
-		request.setType(RequestType.DELETION);
-		request.setDate(timestamp);
-		requestRepository.save(request);
-
-		// Create JSON string
-		String returned = "{" + '"' + "transactionID" + '"' + ":" + '"' + transactionID + '"' + "}";
-		return new ResponseEntity<String>(returned, HttpStatus.OK);
-	}
-
-	/**
-	 * POST /requests/set : set an entity in the blockchain.
-	 *
-	 *
-	 * @param entity the entity to add to the blockchain
-	 * @param value  the value to set the entity to
-	 * @return the ResponseEntity with status 200 (OK) and the transaction ID, or
-	 *         with status 417 (EXPECTATION_FAILED), or with status 500
-	 *         (INTERNAL_SERVER_ERROR)
-	 */
-	@PostMapping("/requests/set")
-	public ResponseEntity<String> setRequest(@RequestParam String entity, String value) {
-		if (entity.isEmpty()) {
-			log.debug("Empty entity name");
-			return new ResponseEntity<String>("EMPTY_ENTITY_NAME", HttpStatus.EXPECTATION_FAILED);
-		}
-		if (value.isEmpty()) {
-			log.debug("Empty value");
-			return new ResponseEntity<String>("EMPTY_VALUE", HttpStatus.EXPECTATION_FAILED);
-		}
-
-		Set blockchainRequest;
-		String transactionID;
-		try {
-			blockchainRequest = new Set(entity, value);
-			blockchainRequest.send();
-			transactionID = blockchainRequest.transactionID;
-		} catch (A_BlockchainException e) {
-			String errored = "BLOCKCHAIN ERROR: " + e.toString();
-			log.debug(errored);
-			return new ResponseEntity<String>(errored, HttpStatus.NOT_ACCEPTABLE);
-		} catch (Exception e) {
-			String errored = "BLOCKCHAIN ERROR: " + e.toString();
-			log.debug(errored);
-			return new ResponseEntity<String>(errored, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-
-		// Log the insert operation (whether fail or not)
-		Instant timestamp = Instant.now();
-		Request request = new Request();
-		request.setValue(value);
-		request.setType(RequestType.ADDITION);
-		request.setDate(timestamp);
-		requestRepository.save(request);
-
-		// Create JSON string
-		String returned = "{" + '"' + "transactionID" + '"' + ":" + '"' + transactionID + '"' + "}";
-		return new ResponseEntity<String>(returned, HttpStatus.OK);
-	}
-`]
+import ${packageName}.web.rest.util.HeaderUtil;\n`]
         }, this);
+
     }
 
     end() {
